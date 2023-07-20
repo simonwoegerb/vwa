2013a582d8b490d9dbe59f0db27eedcc2a676d23
Simon <simonwoegerbauer@outlook.com>
2023-07-20 14:08:36 +0200

    Added List of Figures with Links and continued writing chapter 2
    Signed-off-by: Simon <simonwoegerbauer@outlook.com>

--- clear_files.sh
+++ clear_files.sh
@@ -1,2 +1,3 @@
-rm {title,main,1,2,3}.{pdf,out,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} 2> /dev/null
+rm {title,main,1,2,3}.{.tex,''}{lof,pdf,out,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} 2> /dev/null
+rm main.lof 2> /dev/null
 echo "Cleared Files"
