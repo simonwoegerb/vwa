122f2d0be39736f5951661b6703baf60158f6afe
Simon <simonwoegerbauer@outlook.com>
2023-07-24 09:03:08 +0200

    undid stupid idea and did my first merge, proud!
    Signed-off-by: Simon <simonwoegerbauer@outlook.com>

--- clear_files.sh
+++ clear_files.sh
@@ -1,2 +1,2 @@
-rm {title,main,1,2,3}.{.tex,''}{lof,out,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} main.lof {1,2,3}.pdf 2> /dev/null
+rm {title,main,1,2,3}.{.tex,''}{lof,out,pdf,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} main.lof 2> /dev/null
 echo "Cleared Files"
