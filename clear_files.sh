1007d9f8a655cfc64dbab7573814ba1edf588d45
Simon <simonwoegerbauer@outlook.com>
2023-07-24 08:59:25 +0200

    changed clear_files to keep main.pdf
    Signed-off-by: Simon <simonwoegerbauer@outlook.com>

--- clear_files.sh
+++ clear_files.sh
@@ -1,3 +1,2 @@
-rm {title,main,1,2,3}.{.tex,''}{lof,pdf,out,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} 2> /dev/null
-rm main.lof 2> /dev/null
+rm {title,main,1,2,3}.{.tex,''}{lof,out,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} main.lof {1,2,3}.pdf 2> /dev/null
 echo "Cleared Files"
