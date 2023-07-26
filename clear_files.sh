0e687836d908b3f570a30c8f65a79c59eca23e89
Simon <simonwoegerbauer@outlook.com>
2023-07-26 07:12:02 +0200

    returned tools to root

---
+++ clear_files.sh
@@ -0,0 +1,2 @@
+rm {title,main,1,2,3}.{.tex,''}{lof,out,pdf,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} main.lof 2> /dev/null
+echo "Cleared Files"
--- tools/clear_files.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-rm {title,main,1,2,3}.{.tex,''}{lof,out,pdf,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} main.lof
-echo "Cleared Files"
