78b90b2fed1a6ae0b5ba984478c053006c4a7845
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2023-08-10 10:26:59 +0200

    added abstract,intro and fixed clear_files to actually do its job properly

--- clear_files.sh
+++ clear_files.sh
@@ -1,2 +1,2 @@
-rm {title,main,1,2,3}.{.tex,''}{lof,out,pdf,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} main.lof 2> /dev/null
+rm {title,main,1,2,3}{.tex,'.'}{lof,toc,out,pdf,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg}  2> /dev/null
 echo "Cleared Files"
