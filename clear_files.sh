250d9482a8bcd56dd5108f741b86f12b483dc7d5
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2024-01-03 15:41:14 +0100

    fixed clear_files.sh

--- clear_files.sh
+++ clear_files.sh
@@ -1,2 +1,1 @@
-rm {title,main,1,2,3,4}{.tex.,'.'}{ist, glo,lof,toc,out,pdf,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} 
-echo "Cleared Files"
+rm ./{title,main,1,2,3,4}{.tex.,'.'}{ist,glo,lof,toc,out,pdf,dvi,fdb_latexmk,'synctex.gz',fls,log,'run.xml',bcf,aux,bbl,blg} 2> /dev/null
